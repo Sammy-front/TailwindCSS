72be875abde97da3ddd2a5ace9d88a053be5f939
Sammy-front <sajuan1020@gmail.com>
2025-09-09 11:07:18 -0300

    Header prototipação.

--- Atividades/shadcn/src/app/page.jsx
+++ Atividades/shadcn/src/app/page.jsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Separator } from "@/components/ui/separator"
 import Header from "@/components/layout/header.jsx";
+import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <Header />
+      <Header />
+      <Separator />
     </>
   );
 }
